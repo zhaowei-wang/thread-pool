abe50b8691c0da770ebffac5245243118a01ce7b
zhaoweicharlie_wang <zhaowei_wang@apple.com>
2018-10-15 18:49:10 -0700

    Experimenting with std::thread with a templated functor.

--- src/main.cpp
+++ src/main.cpp
@@ -55,6 +55,45 @@ public:
 end
  */
 
+/*
+Work scheduler
+ */
+template <class work_t, class arg_t, class callback_t>
+class work
+{
+public:
+  work() : unit(nullptr), arg(nullptr), callback(nullptr) {}
+  work(work_t *u, arg_t *a, callback_t *c) : unit(u), arg(a), callback(c) {}
+  
+  void operator()()
+  {
+    if (callback && arg)
+    {
+      (*callback)((*unit)(*arg));
+      return;
+    }
+    else if (callback && !arg)
+    {
+      (*callback)((*unit)());
+      return;
+    }
+    else if (!callback && arg)
+    {
+      (*unit)(*arg);
+      return;
+    }
+    
+    (*unit)();
+  }
+private:
+  work_t *unit;
+  arg_t *arg;
+  callback_t *callback;
+};
+/*
+end
+ */
+
 class concurrent_int : object_lockable<concurrent_int>
 {
 public:
@@ -65,13 +104,45 @@ public:
     lock l(*this);
     i++;
   }
+
+  int value() { return i; }
   
 private:
   int i;
 };
 
+void increment_ci(concurrent_int *ci)
+{
+  ci->increment();
+}
+
 int main(int argc, char * argv[])
 {
-  std::cout << "Hello world!" << std::endl;
+  pthread_t threads[WORK_POOL_COUNT];
+  work<std::function<void()>, void, void> to_do[WORK_POOL_COUNT];
+
+  concurrent_int ci;
+  int N = 100;
+  
+  std::function<void()> f = [&] ()
+	   {
+	     for (size_t i = 0; i < N; ++i)
+	       ci.increment();
+	   };
+  
+  for (size_t i = 0; i < WORK_POOL_COUNT; ++i)
+  {
+    to_do[i] = work<std::function<void()>, void, void>(&f, nullptr, nullptr);
+  }
+  
+  for (size_t i = 0; i < WORK_POOL_COUNT; ++i)
+    pthread_create(&threads[i], nullptr, to_do[i], nullptr);
+
+  for (size_t i = 0; i < WORK_POOL_COUNT; ++i)
+    pthread_join(&threads[i], nullptr);
+
+  std::cout << "expected " << WORK_POOL_COUNT * N << " ";
+  std::cout << "got " << ci.value() << std::endl;
+  
   return 0;
 }
